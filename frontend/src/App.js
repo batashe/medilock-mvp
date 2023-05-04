298f93a23cea5e386fd170fc5e314c29bd8f10fa
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-05-04 16:15:00 +0530

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Dashboard from './pages/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+      {/* <BrowserRouter>
+      <Routes>
+      <Route path="/login" component={Home}/>
+      </Routes>
+      </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
